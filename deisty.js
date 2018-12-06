8a38dae91f5c8de5449cec86880bc45d9bf6091c
Michael Odumosu <michaelodumosu57@gmail.com>
2018-12-06 15:51:56 -0500

    deisty can now close off sections concerend with understanding user query and grabs all the data need for when the deisty engine is activated

--- deisty.js
+++ deisty.js
@@ -17,9 +17,23 @@
           {
             var deisty_site_check = true // checks if we are at the right site
             var deisty_pillow = GM_getValue("pharma_pillow_GM")
-            var deisty_user_pharse = "USER: I want the history of any bed at 10:30 AM on  11:00 AM was it sucessful at 9:55 AM up to 1 year from sleepmattress" // the user phrase deisty has to interpret
+            var deisty_user_pharse = "USER: I want the history of any bed at 10:30 AM on  11:00 AM was it sucessful at 9:55 AM and 9:30 AM up to 1 year from sleepmattress" // the user phrase deisty has to interpret
             var deisty_sites = [] // array of sites if thats what user want
+            if(GM_getValue("deisty_sites") != undefined){ 
+                deisty_sites  = GM_getValue("deisty_sites")
+            }               
             var deisty_sites_description = [] // values contining informaation about where deisty is in evaluating the script
+            if(GM_getValue("deisty_sites_description") != undefined){ 
+                deisty_sites_description  = GM_getValue("deisty_sites_description")
+            }               
+            var deisty_site_url_counter;  //the amount of diffenet webistes, with this number as refence, if the deisty_sites_description[index].at_coorect_sjte equals false while looking, deisty will make sure to navigate to the proper site
+            if(GM_getValue("deisty_site_url_counter") == undefined){ 
+                GM_setValue("deisty_site_url_counter",0)
+                deisty_site_url_counter  = 0 
+            }   
+            else{
+                deisty_site_url_counter  = GM_getValue("deisty_site_url_counter") 
+            }            
             var deisty_counter = 0 // how many times deisty has to loop on a website to make it http compliant
             
             // deisty correct format site handler
@@ -40,8 +54,28 @@
             var deisty_history_available = [new RegExp(/ +?up +?to +?/)]  // this tells up how long ago  we should be looking if not availble throw and error 
             var deisty_history_format = [new RegExp(/ +?up +?to +?\d+ +?(years?|months?|weeks?|days?|infinity|as far back +?((as) +?(you) +?(can))?)/i)] // help deisty understand how far back we need to go
             var deisty_time_rate // time time zone the user is requesting
-            var deisty_time_number// the number of the rate 
-          }
+            var deisty_time_number// the number of the rate        
+
+            // getting to the right page
+            var deisty_dynamic 
+            if(GM_getValue("deisty_dynamic") == undefined){ 
+                GM_setValue("deisty_dynamic",false)
+                deisty_dynamic  = false 
+            }   
+            else{
+                deisty_dynamic  = GM_getValue("deisty_dynamic") 
+            }
+            // tells us that the script is in an active phase and any activities related to the understanding of the user query should be silenced 
+            var deisty_requirements
+            if(GM_getValue("deisty_requirements") != undefined){ 
+                deisty_requirements  = GM_getValue("deisty_requirements")
+            }   
+            // what data is deisty ultimately reuqired to collect or what deisty is supposed to do for each specific relative site firm deisty sites in its index 
+            // deisty dynamaic must be set to true here, the declaration cannot come from here if the  flag is false
+           
+                //DEISTY SITE SEARCHER !!!!
+                var deisty_tags = [] // huge array, dealing with subparrents tags, helping deisty find the website part needed to access and retireve data      
+           }
 
           // functions
           
@@ -76,9 +110,26 @@
               }
               // turns human user requested sites to http format
               // this function should handle one site at a time with flags for complex variations
+
+
+
+              function deisty_SITE_SEARCHER(){
+                   //the most powerful function in this program, this function seraches a webpage through it tags and find the answers where it can locate the data
+                   // actions are taken here if the script needs to get closer to achieveing its goal
+                   // after it sees a pattern its logs how it gets there, 
+                   // and if the pattern falls off it requests the intital handler to search the proper path to the data again
+                   console.group("%cDEISTY SITE SEARCHER !!!!","color:yellow; background-color:red; font:bold; font-size : 2em")
+                       console.deisty("If you CPU gets crushed your problem is most likely here")
+                       for (var deisty_iiii in document.querySelectorAll("body > *")){
+                            console.log()
+                       }
+                   console.groupEnd()
+
+
+              }              
           
           // requests regex handler
-          {
+          if(!deisty_dynamic){
               console.log(deisty_user_pharse)
 
               //
@@ -118,17 +169,11 @@
                           deisty_sites.push(deisty_site_i)
                           deisty_sites_description.push({
                                                             "looking":false
-                              
-                              
-                              
                                                                     })
                           console.deisty("the sites I have so far")
                           console.deisty_data(deisty_sites)
 
                       }
-                      
-
-
 
                   }
                   else{
@@ -196,12 +241,8 @@
                   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-                
               }
               
-
               if(deisty_user_pharse.match("history").index != -1  ){
                   console.group("determining time zone")
                   console.deisty("so you are interested in the history of something ")
@@ -213,7 +254,7 @@
                             console.log("deisty_time_match_at_result", deisty_time_match_at_result) 
                             if(deisty_user_pharse.match(deisty_time_match_sucessful[0]).index != -1  ){
                                 console.group("deisty time sucessful")
-                                console.log(deisty_user_pharse.match(deisty_time_match_sucessful[0])[0])
+                                console.log(deisty_user_pharse.match(deisty_time_match_sucessful[0]))
                                 deisty_time_match_sucessful_result.push(deisty_user_pharse.match(deisty_time_match_sucessful[0])[0])
                                 console.log("deisty_time_match_sucessful_result",deisty_time_match_sucessful_result)
                                 for(var deisty_ii in deisty_time_match_sucessful_result){
@@ -221,7 +262,7 @@
                                     deisty_time_match_sucessful_counter = deisty_time_match_sucessful_result[deisty_ii]
                                     console.log(deisty_time_match_sucessful_counter.match(deisty_time_format[0])) 
                                     deisty_time_match_sucessful_counter = deisty_time_match_sucessful_counter.match(deisty_time_format[0])
-                                    deisty_time_match_sucessful_result[deisty_ii] = deisty_time_match_sucessful_counter[0]
+                                    deisty_time_match_sucessful_result[deisty_ii] = deisty_time_match_sucessful_counter
                                 }
                                 console.deisty("so was it good or bad to consider from these times is your question")
                                 console.log(deisty_time_match_sucessful_result)
@@ -244,24 +285,60 @@
                                     console.log(deisty_user_pharse.match(deisty_history_format[0])[0])
                                     console.log(deisty_user_pharse.match(deisty_history_format[0])[1])
                                     deisty_time_rate = deisty_user_pharse.match(deisty_history_format[0])[1]
-                                    deisty_time_number =  deisty_user_pharse.match(deisty_history_format[0])[0].match(/\d+/)
+                                    deisty_time_number =  deisty_user_pharse.match(deisty_history_format[0])[0].match(/\d+/)[0]
+                                    console.log(deisty_time_number)
                                     
                                     
-                                      if(deisty_time_rate.match(/years?/i)){ 
-                                            console.log("year")
-                                        }
-                                      if(deisty_time_rate.match(/months?/i)){
-                                            console.log("month")
-                                        }
-                                      if(deisty_time_rate.match(/weeks?/i)){
-                                            console.log("month")
-                                        }
-                                      if(deisty_time_rate.match(/days?/i)){
-                                            console.log("month")
-                                        }          
-                                      if(deisty_time_rate.match(/as|infinity/i)){
-                                        console.log("infinity")
-                                        }                                                                                                                            
+                                    if(deisty_time_rate.match(/years?/i)){ 
+                                          console.log("year")
+                                          deisty_time_number = parseInt(deisty_time_number) * 365 
+                                      }
+                                    if(deisty_time_rate.match(/months?/i)){
+                                          console.log("month")
+                                          deisty_time_number = parseInt(deisty_time_number) * 30
+                                      }
+                                    if(deisty_time_rate.match(/weeks?/i)){
+                                          console.log("weeks")
+                                          deisty_time_number = parseInt(deisty_time_number) * 7
+                                      }
+                                    if(deisty_time_rate.match(/days?/i)){
+                                          console.log("days")
+                                          deisty_time_number = parseInt(deisty_time_number) * 1
+                                      }          
+                                    if(deisty_time_rate.match(/as|infinity/i)){
+                                      console.log("infinity")
+                                      deisty_time_rate = "infinity"
+                                      }
+
+                                    console.deisty("This is how far I am going back then huh \n" +  deisty_time_number)
+                                    console.deisty("Anything specific?")
+
+                                    
+                                    if(deisty_user_pharse.match(/especi|specific(ally)?/)){
+
+                                      // response code here
+                                    }
+                                    else{
+
+                                        console.group((" DEISTY: ok these are my time values saving to GM"))
+                                        console.log("how many days" ,deisty_time_number)
+                                        console.log("future point" , deisty_time_match_at_result)
+                                        console.log("past point" , deisty_time_match_sucessful_result)
+
+                                        deisty_requirements = []
+                                        deisty_requirements.push( {
+                                                        "how many days" :deisty_time_number,
+                                                        "future point" : deisty_time_match_at_result,
+                                                        "past point" : deisty_time_match_sucessful_result
+                                                      })
+                                        // nothing should be set to GM until deisty understand everything it needs from the site, different from GM
+                                        // logging of the site theirselves 
+                                        GM_setValue("deisty_requirements",deisty_requirements)                                                      
+                                        console.groupEnd()                                          
+                                    }
+
+
+
                                
                                     
                                 }
@@ -271,28 +348,65 @@
                   }
                   catch(e){
                       console.log(e)
-                      throw("error, I dont understanding what the user wants concerning time ")
+                      console.error("error, I dont understanding what the user wants concerning time ")
+                      console.deisty("I am a mean program so this is how I want things done")
+                      throw("USER: I want the history of any bed at [as many times in hh:mm (12) hr format, also include and between things!]   was it sucessful at [as many times in hh:mm (12) hr format, also include and between things!] up to [how far you want me to go back in days ] from [one website]")
                   }                  
                   console.groupEnd()
               }
 
-
-
-
-
-
-
-
-
-
               if(deisty_user_pharse.match("bed").index != -1){
+                      console.group("beds ")
                       console.deisty("so you are interested in beds ill pull up the list")
                       deisty_pillow = GM_getValue("pharma_pillow_GM")     // pillow we have to work with
+                      if(deisty_user_pharse.match("any +?bed") != null){
+
+                        // response code here
+                      }
+                      else{
+                        console.deisty("so you want all the pillow that my partner has ok")
+                      }
                       console.deisty_data( deisty_pillow)
+
+                      console.groupEnd()
               }
 
           }
           // this section handles the desires understanding of the user
+
+          console.group("getting to the right page")
+              console.deisty("so this is what I am supposed to ")
+              console.info(deisty_requirements)
+              console.deisty("first lets activate dynamic mode ")
+              deisty_dynamic = true
+              GM_setValue("deisty_dynamic",deisty_dynamic)
+              console.deisty("am I at the right site")
+              console.log(deisty_sites[deisty_site_url_counter])
+              console.log(deisty_sites_description[deisty_site_url_counter])
+
+
+
+
+              console.deisty("so now how do I search on this site, is there a search bar where I can enter beds")
+
+              deisty_SITE_SEARCHER()
+              
+
+          console.groupEnd()    
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////6/////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
